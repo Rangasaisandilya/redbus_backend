03ef0e32eb63eae296789473a19f3c6b49873fde
Manicka Vasagar N <manickavasagar.n@hcltech.com>
2025-07-10 16:59:35 +0530

    change api controller

--- src/controllers/busController.ts
+++ src/controllers/busController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import Bus from "../models/Bus";
 import { sendResponse } from "../utils/response";
+import { FilterQuery } from "mongoose";
 
 export const addBus = async (req: Request, res: Response) => {
   try {
@@ -20,13 +21,42 @@ export const addBus = async (req: Request, res: Response) => {
   }
 };
 
-export const getAllBuses = async (_req: Request, res: Response) => {
+export const getAllBuses = async (req: Request, res: Response) => {
   try {
-    const buses = await Bus.find().populate("owner driver", "name email");
+    const page = parseInt((req.query.page as string) || "1", 10);
+    const limit = parseInt((req.query.limit as string) || "10", 10);
+    const search = (req.query.search as string) || "";
+    const owner = (req.query.owner as string) || "";
+    const driver = (req.query.driver as string) || "";
+
+    const query: FilterQuery<any> = {}; // Search by license number or contact
+
+    if (search) {
+      query.$or = [
+        { license_number: { $regex: search, $options: "i" } },
+        { contact: { $regex: search, $options: "i" } },
+      ];
+    } // Filter by owner ID
+
+    if (owner) {
+      query.owner = owner;
+    } // Filter by driver ID
+
+    if (driver) {
+      query.driver = driver;
+    }
+
+    const buses = await Bus.find(query)
+      .populate("owner driver", "name email")
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    const total = await Bus.countDocuments(query);
     sendResponse(res, 200, {
       status: true,
       message: "Bus Fetch successfully",
       data: buses,
+      total:total
     });
   } catch (err) {
     sendResponse(res, 500, {
